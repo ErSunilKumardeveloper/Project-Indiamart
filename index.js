6387a86f01e38c34f46f49251b48d1769b79b1e2
Sunil Kumar <sunilkumar55066@gmail.com>
2022-10-12 19:14:38 +0530

    code

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    
+}
+
